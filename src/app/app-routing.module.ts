3b77dd814184787f582deb844a18c7ef26e26775
Oscar Pallazhco <oepallaz@gmail.com>
2022-02-11 08:42:04 -0500

    add an admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProtectedPageComponent } from './components/protected-page/protected-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'protected', component: ProtectedPageComponent},
+  {path: 'admin', component: AdminPageComponent},
   {path: '**', pathMatch: 'full', redirectTo:'home'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProtectedPageComponent } from './components/protected-page/protected-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NavbarComponent,
-    ProtectedPageComponent
+    ProtectedPageComponent,
+    AdminPageComponent
   ],
   imports: [
     BrowserModule,
